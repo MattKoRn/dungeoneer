8dfc297ff1fb4e71def443d2440036f186c4dda3
mtitus <mtitus@b0c3249a-3d3c-0410-baef-f95418684d86>
2009-07-15 20:31:41 +0000

    Fix crash when the upper left tile is a wall. Added logging. Player now moves to the stairs if he is adjacent to it. If the player gets back to his original position and all the other nearby spaces have already been visited, reset the visited list to keep him from getting stuck in a loop of moving between the first two squares.

---
+++ trunk/MazeScreenSaver/MazeScreenSaver/Logger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace MazeScreenSaver
+{
+    class Logger
+    {
+        private string logfile = @"c:\dungeoneer\log.txt";
+
+        public Logger()
+        {
+        }
+
+        public void Write(string message)
+        {
+            if (!Directory.Exists(Directory.GetParent(logfile).ToString()))
+                Directory.CreateDirectory(Directory.GetParent(logfile).ToString());
+            StreamWriter file = new StreamWriter(logfile, true);
+            file.WriteLine(message);
+            file.Close();
+        }
+    }
+}
--- trunk/MazeScreenSaver/MazeScreenSaver/Player.cs
+++ trunk/MazeScreenSaver/MazeScreenSaver/Player.cs
@@ -15,6 +15,8 @@ namespace MazeScreenSaver
         private List<Point> visitedCoords;
         private Point oldestBacktrack;
         private bool is_backtracking;
+        public bool has_moved = false;
+        public Point stairsCoord;
 
         public Player()
         {
@@ -42,23 +44,36 @@ namespace MazeScreenSaver
                     }
                 }
             }
+
+            if (preferredMoves.Count == 0 && oldestBacktrack == visitedCoords[1] && coord == visitedCoords[0])
+            {
+                // Reset the list to keep us from getting in a loop. By this point actually we would have to give up but we're moving randomly so it can happen.
+                visitedCoords = new List<Point>();
+                visitedCoords.Add(coord);
+                setAvailableMoves(moves); // Try again
+            }
         }
 
         public void move()
         {
             Random r = new Random();
             previousCoord = coord;
+            if (preferredMoves.Contains(stairsCoord))
+            {
+                coord = stairsCoord;
+            }
             if (preferredMoves.Count() > 0)
             {
                 coord = preferredMoves[r.Next(preferredMoves.Count())];
                 visitedCoords.Add(coord);
-                is_backtracking = true;
+                is_backtracking = false;
             }
             else
             {
                 coord = oldestBacktrack;
                 is_backtracking = true;
             }
+            has_moved = true;
         }
     }
 }
