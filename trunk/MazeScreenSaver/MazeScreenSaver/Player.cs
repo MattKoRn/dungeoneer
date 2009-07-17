9b81a489185a19ded6ea1bc57814ea1363ee2614
mtitus <mtitus@b0c3249a-3d3c-0410-baef-f95418684d86>
2009-07-17 16:08:15 +0000

    Add directions class. The player now tries to keep moving in the same direction as his previous move, unless he moves backwards. If he can't move in exactly the same direction, he tries to pick the direction closest to his previous move.

--- trunk/MazeScreenSaver/MazeScreenSaver/Player.cs
+++ trunk/MazeScreenSaver/MazeScreenSaver/Player.cs
@@ -18,12 +18,16 @@ namespace MazeScreenSaver
         public bool has_moved = false;
         public Point stairsCoord;
         private Random rand;
+        private Direction facing;
+        private int rotationDirection;
 
         public Player()
         {
             coord = new Point(0, 0);
             visitedCoords = new List<Point>();
             rand = new Random();
+            facing = Direction.None;
+            rotationDirection = rand.Next(1, 3); // 1 or 2, 1 = clockwise, 2 = counterclockwise
         }
 
         public void setAvailableMoves(List<Point> moves)
@@ -59,20 +63,83 @@ namespace MazeScreenSaver
         public void move()
         {
             previousCoord = coord;
+            List<List<Point>> goodMoves = new List<List<Point>>();
+            goodMoves.Add(new List<Point>(1));
+            goodMoves.Add(new List<Point>(2));
+            goodMoves.Add(new List<Point>(2));
+            goodMoves.Add(new List<Point>(2));
+            goodMoves.Add(new List<Point>(1));
             if (preferredMoves.Contains(stairsCoord))
             {
                 coord = stairsCoord;
-            }
-            if (preferredMoves.Count() > 0)
-            {
-                coord = preferredMoves[rand.Next(preferredMoves.Count())];
-                visitedCoords.Add(coord);
                 is_backtracking = false;
+            }
+
+            if (preferredMoves.Count() == 0)
+            {
+                coord = oldestBacktrack;
+                facing = Direction.getDirection(coord, previousCoord);
+                is_backtracking = true;
             }
             else
             {
-                coord = oldestBacktrack;
-                is_backtracking = true;
+                if (facing == Direction.None)
+                {
+                    Point move = preferredMoves[rand.Next(preferredMoves.Count)];
+                    coord = move;
+                    facing = Direction.getDirection(previousCoord, coord);
+                    is_backtracking = false;
+                }
+
+                // Keep moving in the same direction if you can
+                foreach (Point move in preferredMoves)
+                {
+                    if (Direction.getDirection(coord, move).delta == facing.delta)
+                    {
+                        List<Point> sameDir = new List<Point>();
+                        goodMoves[0].Add(move);
+                    }
+                    else if (Direction.getDirection(coord, move).delta == facing.adjacent[0].delta ||
+                        Direction.getDirection(coord, move).delta == facing.adjacent[1].delta)
+                    {
+                        goodMoves[1].Add(move);
+                    }
+                    else if (Direction.getDirection(coord, move).delta == facing.adjacent[0].adjacent[0].delta ||
+                        Direction.getDirection(coord, move).delta == facing.adjacent[1].adjacent[1].delta)
+                    {
+                        goodMoves[2].Add(move);
+                    }
+                    else if (Direction.getDirection(coord, move).delta == facing.adjacent[0].adjacent[0].adjacent[0].delta ||
+                        Direction.getDirection(coord, move).delta == facing.adjacent[1].adjacent[1].adjacent[1].delta)
+                    {
+                        goodMoves[3].Add(move);
+                    }
+                    else if (Direction.getDirection(coord, move).delta == facing.adjacent[0].adjacent[0].adjacent[0].adjacent[0].delta) // should be the same as 4x[1]
+                    {
+                        goodMoves[4].Add(move);
+                    }
+                }
+
+                Point moveTo = preferredMoves[rand.Next(preferredMoves.Count)];
+                foreach (List<Point> movelist in goodMoves)
+                {
+                    if (movelist.Count == 0)
+                        continue;
+                    if (movelist.Count == 1)
+                    {
+                        moveTo = movelist[0];
+                        break;
+                    }
+                    else
+                    {
+                        moveTo = movelist[rand.Next(movelist.Count)];
+                        break;
+                    }
+                }
+                coord = moveTo;
+                visitedCoords.Add(coord);
+                facing = Direction.getDirection(previousCoord, coord);
+                is_backtracking = false;
             }
             has_moved = true;
         }
