458401b719fdd7ad901f301e5861085b4cc72c20
mtitus <mtitus@b0c3249a-3d3c-0410-baef-f95418684d86>
2009-07-24 14:05:09 +0000

    Added "Show Visited Trails" to option screen. Also encapsulated registry functions.

--- MazeScreenSaver/MazeScreenSaver/OptionsForm.cs
+++ MazeScreenSaver/MazeScreenSaver/OptionsForm.cs
@@ -12,21 +12,40 @@ namespace MazeScreenSaver
     public partial class OptionsForm : Form
     {
         public string[] prgParams;
+        private string registryKey = "HKEY_CURRENT_USER\\Software\\Dungeoneer";
 
         public OptionsForm()
         {
             InitializeComponent();
         }
 
+        private void setCheckboxFromRegistry(CheckBox check, string valueName)
+        {
+            object value = Microsoft.Win32.Registry.GetValue(registryKey, valueName, null);
+            if (value != null)
+            {
+                if ((int)value == 1)
+                    check.Checked = true;
+                else
+                    check.Checked = false;
+            }
+            else
+                check.Checked = false;
+
+        }
+
         private void OptionsForm_Load(object sender, EventArgs e)
         {
-            if (Microsoft.Win32.Registry.GetValue("HKEY_CURRENT_USER\\Software\\Dungeoneer", "enableLogging", null) != null)
-                if ((int)Microsoft.Win32.Registry.GetValue("HKEY_CURRENT_USER\\Software\\Dungeoneer", "enableLogging", null) == 1)
+            if (Microsoft.Win32.Registry.GetValue(registryKey, "enableLogging", null) != null)
+                if ((int)Microsoft.Win32.Registry.GetValue(registryKey, "enableLogging", null) == 1)
                     check_enableLogging.Checked = true;
                 else
                     check_enableLogging.Checked = false;
             else
                 check_enableLogging.Checked = false;
+
+            setCheckboxFromRegistry(check_enableLogging, "enableLogging");
+            setCheckboxFromRegistry(checkTrails, "enableTrails");
         }
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
@@ -34,15 +53,21 @@ namespace MazeScreenSaver
 
         }
 
-        private void btnOK_Click(object sender, EventArgs e)
+        private void setRegistryValueFromCheckbox(CheckBox check, string valueName)
         {
             int value;
-            if (check_enableLogging.Checked)
+            if (check.Checked)
                 value = 1;
             else
                 value = 0;
 
-            Microsoft.Win32.Registry.SetValue("HKEY_CURRENT_USER\\Software\\Dungeoneer", "enableLogging", value);
+            Microsoft.Win32.Registry.SetValue(registryKey, valueName, value);
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            setRegistryValueFromCheckbox(check_enableLogging, "enableLogging");
+            setRegistryValueFromCheckbox(checkTrails, "enableTrails");
             Close();
         }
 
